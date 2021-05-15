a6241eac3e545d76cda8a69a9f63239d96424fe4
ljj2582021239 <2582021239@qq.com>
2021-05-16 02:46:14 +0800

    master commit 2

--- src/com/indiv/basis/StringTes1.java
+++ src/com/indiv/basis/StringTes1.java
@@ -10,5 +10,6 @@ public class StringTes1 {
         System.out.println("hello git version 3");
 
         System.out.println("hello git version 4");
+        System.out.println("master commit test");
     }
 }
