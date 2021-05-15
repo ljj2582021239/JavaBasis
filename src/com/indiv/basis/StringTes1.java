054a24ddb1608900d2e1176449f63b0048f65cff
ljj2582021239 <2582021239@qq.com>
2021-05-16 02:31:41 +0800

    second commit

--- src/com/indiv/basis/StringTes1.java
+++ src/com/indiv/basis/StringTes1.java
@@ -1,4 +1,8 @@
 package com.indiv.basis;
 
 public class StringTes1 {
+    public static void main(String[] args) {
+        System.out.println("hello git version 1");
+        System.out.println("hello git version 2");
+    }
 }
