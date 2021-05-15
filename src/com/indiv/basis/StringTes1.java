15719cbed9ef9b4b8a567f0076309204b117e66d
ljj2582021239 <2582021239@qq.com>
2021-05-16 02:41:23 +0800

    hot-fix commit 1

--- src/com/indiv/basis/StringTes1.java
+++ src/com/indiv/basis/StringTes1.java
@@ -1,9 +1,14 @@
 package com.indiv.basis;
 
+/**
+ * 使用IDEA 练习 Git
+ */
 public class StringTes1 {
     public static void main(String[] args) {
         System.out.println("hello git version 1");
         System.out.println("hello git version 2");
         System.out.println("hello git version 3");
+
+        System.out.println("hello git version 4");
     }
 }
